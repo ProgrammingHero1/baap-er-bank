06445e175c3481962c2884032ee9a98aa64ab821
Programming <programming.hero1@gmail.com>
2022-08-12 21:20:12 -0500

    created the login page

---
+++ js/login.js
@@ -0,0 +1,22 @@
+// step-1: add click event handler with the submit button
+document.getElementById('btn-submit').addEventListener('click', function(){
+    // step-2: get the email address inside the email input field
+    // always remember to use .value to get text from an input field
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+    // step-3: get password
+    // 3.a: set id on the html element 
+    // 3.b: get the element
+    // 3.c: get the value from the element
+    const passwordField = document.getElementById('user-password');
+    const password = passwordField.value;
+    
+    // DANGER: DO NOT VERIFY email password on the client side 
+    // step-4: verify email and password and check whether valid user or not
+    if(email === 'sontan@baap.com' && password === 'secret'){
+        console.log('valid user')
+    }
+    else{
+        console.log('invalid user')
+    }
+})
