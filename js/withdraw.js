d36e75657570802751fc8a590d2b10504bd947f3
Programming <programming.hero1@gmail.com>
2022-08-12 23:49:16 -0500

    handle error

--- js/withdraw.js
+++ js/withdraw.js
@@ -2,6 +2,7 @@
 1. add event handler with the withdraw button
 2. get the withdraw amount from the withdraw input field
 2-5. also make sure to convert the input into a number by using parseFloat
+
 3. Get previous withdraw total
 
 4. calculate total withdraw amount 
@@ -15,30 +16,45 @@
 */
 
 // step-1
-document.getElementById('btn-withdraw').addEventListener('click', function(){
+document.getElementById('btn-withdraw').addEventListener('click', function () {
     // step-2:
     const withdrawField = document.getElementById('withdraw-field');
     const newWithDrawAmountString = withdrawField.value;
     const newWithdrawAmount = parseFloat(newWithDrawAmountString);
-    
+
+    // step-7
+    withdrawField.value = '';
+
+    if (isNaN(newWithdrawAmount)) {
+        alert('Please provide a valid number');
+        return;
+    }
+
     // step-3
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const previousWithdrawTotalString = withdrawTotalElement.innerText;
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
-    
-    // step-4
-    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-    withdrawTotalElement.innerText = currentWithdrawTotal;
+
 
     // step-5
     const balanceTotalElement = document.getElementById('balance-total');
     const previousBalanceTotalString = balanceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
 
+
+    if (newWithdrawAmount > previousBalanceTotal) {
+        alert('Baap er bank e eto taka nai');
+        return;
+    }
+
+    // step-4
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
+
+
     // step-6
     const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
     balanceTotalElement.innerText = newBalanceTotal;
 
-    // step-7
-    withdrawField.value = '';
+
 })
