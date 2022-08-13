1d309b9a6a01cfb248d554c6bf2630e2cece18fb
Programming <programming.hero1@gmail.com>
2022-08-12 21:40:48 -0500

    added deposit, withdraw and balance section

--- js/login.js
+++ js/login.js
@@ -14,9 +14,9 @@ document.getElementById('btn-submit').addEventListener('click', function(){
     // DANGER: DO NOT VERIFY email password on the client side 
     // step-4: verify email and password and check whether valid user or not
     if(email === 'sontan@baap.com' && password === 'secret'){
-        console.log('valid user')
+        window.location.href = 'bank.html';
     }
     else{
-        console.log('invalid user')
+        alert('Tui password vule gesos!! Toke ami teijjo sontan gosona korlam.')
     }
 })
